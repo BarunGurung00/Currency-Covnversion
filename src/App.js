af42eebd83e453e69607bc74e0f5b4f753ce7f97
barungurung00@gmail.com <barun>
2024-01-22 05:44:57 +0000

    Getting the token for live updated data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-
-import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+const [currencyOptions, setCurrencyOptions] = useState([])
+
+useEffect(() => {
+  fetch("https://api.currencyapi.com/v3/latest?apikey=cur_live_zdok13Vm6bcqOwsrbPV1DUqMRcycFLaXtFuyWqEw")
+  .then(res => res.json())
+  .then(data => {
+          const keys = Object.keys(data.data)
+          const values = Object.values(data.data)
+          for(let value of values){
+            console.log(value.value,)
+          }
+  })
+          
+}, [])
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1 className="">Convert any currency</h1>
+      <div>
+        <input type="number" />
+        <select>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="CAD">CAD</option>
+        </select>
+      </div>
+      <div>
+        <input type="number" />
+        <select>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="CAD">CAD</option>
+        </select>
+      </div>
     </div>
   );
 }
